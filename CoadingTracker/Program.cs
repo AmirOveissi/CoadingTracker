d23bc3ea187132ce64c8244d83679d182d9c2b0a
AmirOveissi <a.oveissi75@gmail.com>
2024-07-18 13:26:38 +0300

    I wrote a method

--- CoadingTracker/Program.cs
+++ CoadingTracker/Program.cs
@@ -1,4 +1,6 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, C# Academy! one more modification"); Console.WriteLine("Hello, C# Academy! one more modification"); Console.WriteLine("Hello, C# Academy! one more modification"); Console.WriteLine("Hello, C# Academy! one more modification");
-Console.WriteLine("Hello, C# Academy! one more modification"); Console.WriteLine("Hello, C# Academy! one more modification"); Console.WriteLine("Hello, C# Academy! one more modification");
-Console.WriteLine("Hello, C# Academy! one more modification"); Console.WriteLine("Hello, C# Academy! one more modification"); Console.WriteLine("Hello, C# Academy! one more modification");
+﻿MyMethod();
+
+void MyMethod()
+{
+    Console.WriteLine("Now I have method");
+}
